f9a56d69c40c7b7bc2bb03d11803b889ba21dae6
russelibale808 <russelibale808@gmail.com>
2022-10-28 05:08:17 -1000

    CarLostSimulatorExercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public string Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string CarHorn { get; set; }
+        public bool IsDrivable { get; set; }
+
+        // My default constructor
+        public Car()
+        {
+
+        }
+
+        // My customer Constructor
+        public Car( string year, string make, string model, string engineNoise, string carHorn, bool isDrivable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            CarHorn = carHorn;
+            IsDrivable = isDrivable;
+
+        }
+
+        // Methods
+        public void TurnOnEngine(string engineNoise)
+        {
+            Console.WriteLine($"Engine geos {EngineNoise}");
+        }
+
+        public void CarBeep(string carHorn)
+        {
+            Console.WriteLine($"Car horn beeps {CarHorn}");
+        }
+
+    }
+   
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -13,6 +13,36 @@ namespace CarLotSimulator
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
 
+            Car myFirstCar = new Car();
+           // my Dot Notation
+            myFirstCar.Make = "Nissan";
+            myFirstCar.Model = "Sentra SR";
+            myFirstCar.Year = "2014";
+            myFirstCar.EngineNoise = "beeeeeeep";
+            myFirstCar.IsDrivable = true;
+
+            
+
+            myFirstCar.TurnOnEngine(myFirstCar.EngineNoise);
+            myFirstCar.CarBeep(myFirstCar.CarHorn);
+
+            // notes Object Innitializer Syntax
+            var mySecondCar = new Car() 
+            {   Year  = "1994",
+                Make = "Toyota",
+                Model = "Corola",
+                IsDrivable = true 
+            };
+
+            
+
+            mySecondCar.TurnOnEngine("Vrrrrooom");
+            mySecondCar.CarBeep(mySecondCar.CarHorn);
+
+            var myThirdCar = new Car("2008", "Nissan", "240SX", "spurrrrt", "honk honk", false);
+
+            myThirdCar.TurnOnEngine(myThirdCar.EngineNoise);
+            myThirdCar.CarBeep(myThirdCar.CarHorn);
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
@@ -28,6 +58,9 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            
+
         }
     }
 }
