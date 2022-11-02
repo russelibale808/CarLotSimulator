01a07dc578bbb0209a15256e9a856484a086ec61
russelibale808 <russelibale808@gmail.com>
2022-11-01 19:15:45 -1000

    CarLotSimulatorUpdate

---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class CarLot
+    {
+        public CarLot()
+        {
+
+        }
+
+        public List<Car> ParkingLot = new List<Car>();
+
+        public static int numberOfCars = 0;
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -14,7 +14,8 @@ namespace CarLotSimulator
             //The methods should take one string parameter: the respective noise property
 
             Car myFirstCar = new Car();
-           // my Dot Notation
+            CarLot.numberOfCars++;
+            // my Dot Notation
             myFirstCar.Make = "Nissan";
             myFirstCar.Model = "Sentra SR";
             myFirstCar.Year = "2014";
@@ -33,16 +34,17 @@ namespace CarLotSimulator
                 Model = "Corola",
                 IsDrivable = true 
             };
-
+            CarLot.numberOfCars++;
             
 
             mySecondCar.TurnOnEngine("Vrrrrooom");
             mySecondCar.CarBeep(mySecondCar.CarHorn);
 
             var myThirdCar = new Car("2008", "Nissan", "240SX", "spurrrrt", "honk honk", false);
-
+            CarLot.numberOfCars++;
             myThirdCar.TurnOnEngine(myThirdCar.EngineNoise);
             myThirdCar.CarBeep(myThirdCar.CarHorn);
+            
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
@@ -59,8 +61,8 @@ namespace CarLotSimulator
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
 
-            
 
+            Console.WriteLine($"Cars in lot {CarLot.numberOfCars}");
         }
     }
 }
